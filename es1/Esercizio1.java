f306bfe17d9774df2bb178fa208c92e40465e1b8
Simone Samoggia <simone.samoggia2@studio.unibo.it>
2024-08-09 11:20:37 +0200

    funziona l'equals tra alberi

--- es1/Esercizio1.java
+++ es1/Esercizio1.java
@@ -4,6 +4,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -177,30 +178,16 @@ public class Esercizio1 {
     }
 
     static boolean areTreesEqual(TreeNode root1, TreeNode root2) {
-        // if (root1 == null && root2 == null)
-        // return true;
-        // if (root1 == null || root2 == null) {
-        // System.out.println("c'è un null");
-        // return false;
-        // }
-        // if (root1.value != root2.value) {
-        // System.out.println("Valori diversi");
-        // return false;
-        // }
-
-        // // Compare children ignoring the order
-        // if (root1.children.size() != root2.children.size())
-        // return false;
 
         // Sort and compare children by converting them into sets
-        Set<Integer> childSet1 = new TreeSet<>();
+        TreeSet<TreeNode> childSet1 = new TreeSet<>();
         for (TreeNode child : root1.children) {
-            childSet1.add(child.value);
+            childSet1.add(child);
         }
 
-        Set<Integer> childSet2 = new TreeSet<>();
+        TreeSet<TreeNode> childSet2 = new TreeSet<>();
         for (TreeNode child : root2.children) {
-            childSet2.add(child.value);
+            childSet2.add(child);
         }
 
         return recAreEquals(childSet1, childSet2);
@@ -214,10 +201,30 @@ public class Esercizio1 {
      * @param childSet2
      * @return
      */
-    static boolean recAreEquals(Set<Integer> childSet1, Set<Integer> childSet2) {
+    static boolean recAreEquals(TreeSet<TreeNode> childSet1, TreeSet<TreeNode> childSet2) {
 
         if (childSet1.size() != childSet2.size()) {
             return false;
+        } else if (childSet1.size() < 1 || childSet2.size() < 1) {
+            return true;
+        }
+
+        try {
+
+            if (childSet1.equals(childSet2)) {
+                if (areTreesEqual(childSet1.first(), childSet2.first())) {
+                    return true;
+                }
+            }
+        } catch (NullPointerException e) {
+            System.out.println("NullPointer: " + e.getMessage());
+            return false;
+        } catch (NoSuchElementException e) {
+            System.out.println("Set is empty: " + e.getMessage());
+            return false;
+        } catch (Exception e) {
+            System.out.println("Errore: " + e.getMessage());
+            e.printStackTrace();
         }
 
         return false;
@@ -234,24 +241,17 @@ public class Esercizio1 {
         String pairList = "es1/parent_child_pairs.txt";
         String nestedList = "es1/nested2.txt";
 
-        try {
-            TreeNode tree1 = buildTreeFromPairs(pairList);
-            TreeNode tree2 = buildTreeFromNestedList(nestedList);
-            printTree(tree1);
-            System.out.println();
-            System.out.println();
-            printTree(tree2);
+        TreeNode tree1 = buildTreeFromPairs(pairList);
+        TreeNode tree2 = buildTreeFromNestedList(nestedList);
+        printTree(tree1);
+        System.out.println();
+        System.out.println();
+        printTree(tree2);
 
-            if (areTreesEqual(tree1, tree2)) {
-                System.out.println("Alberi uguali");
-            } else {
-                System.out.println("Alberi diversi");
-            }
-
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (areTreesEqual(tree1, tree2)) {
+            System.out.println("Alberi uguali");
+        } else {
+            System.out.println("Alberi diversi");
         }
 
     }
